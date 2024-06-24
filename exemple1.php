8717e0f84b1253fcd46c688bbc08cecfbbe4ed18
JC <jessica.l.conrad@gmail.com>
2024-06-24 15:49:01 +0200

    played with file, fixed some comments

--- exemple1.php
+++ exemple1.php
@@ -5,18 +5,24 @@ print("Hello Again!\n");
 
 $nom = readline("Tapez votre nom: ");
 $age = 46;
-print("\nJ'ai " . $age . " ans");
-print("\nJ'ai $age ans");
-print('\nJ\'ai $age ans');// if you need to except a character from being thought of as code
+print("\nJ'ai " . $age . " ans.");//we can concatenate with points
+print("\nJ'ai $age ans.\n");// or php can do implicit concatenation with variables in the phrase
+
+print('\nJ\'ai $age ans.');//we can see simple quotes aren't great in php...
 
 print("\nJe suis " . $nom . " et j'ai " . $age . " ans");
 
-$age = (int)readline("Tapez votre age");
+$age = (int)readline("\nTapez votre age: ");//the (int) here converts the input to an integer
+
+$agePlusOne = $age + 1;
+
 $interdit = true;
-print ("\n" . $interdit);
+print("\n" . $interdit);//a variable that is true when asked to print will print as 1
+
+print("\n $interdit again!  In one year you will be $agePlusOne!");
 
 if($interdit == true){
-    print("\nc'est vrai!"); //this is a comment
+    print("\nc'est vrai!");
 }
 
 if (true){ // in this case we are using the if like a manual switch.
@@ -25,10 +31,10 @@ if (true){ // in this case we are using the if like a manual switch.
 
 
 if ($age < 18){
-    print ("vous êtes trop jeune");
+    print ("\nVous êtes trop jeune");
 }
 elseif ($age > 100){
-    print("\nvous êtes trop vieux!");
+    print("\nVous êtes trop vieux!");
 }
 else {
     print ("\nBienvenue!");

66cf1de959609d68d89387f5bc55680c23b5a208
Jessica C <j.conrad@interface3.be>
2024-09-03 16:29:10 +0200

    fin jour modele et début d'essai projet shoetrack

---
+++ Symfony7Cours/ProjetModeleSymfony/migration.bat
@@ -0,0 +1,16 @@
+echo coucou
+@REM this batch is a way of erasing all old versions and then migrating the version in which we are working to the versions (migrations files)
+@REM effacer les fichiers de migration (versions)
+del migrations\Version*
+
+@REM effacer la BD
+symfony console doctrine:database:drop --force --no-interaction
+
+@REM creer la BD
+symfony console doctrine:database:create
+
+@REM creer une migration de nos données courantes
+symfony console make:migration --no-interaction
+
+@REM lancer la migration
+symfony console doctrine:migrations:migrate --no-interaction

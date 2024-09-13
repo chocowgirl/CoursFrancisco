5c9125df6015ae500a9c59c651a686b8f0a407e2
Jessica C <j.conrad@interface3.be>
2024-09-13 12:16:25 +0200

    morning with Francisco

--- Symfony7Cours/ProjetFormulaireSymfony/src/Controller/FormulairesController.php
+++ Symfony7Cours/ProjetFormulaireSymfony/src/Controller/FormulairesController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Aeroport;
 use App\Form\AeroportType;
+use App\Repository\AeroportRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\ManagerRegistry as DoctrineManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,6 +15,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FormulairesController extends AbstractController
 {
+    public ManagerRegistry $doctrine;
+
+    //injecter le managerregistry dans le constructeur
+    public function __construct(ManagerRegistry $doctrine){
+        $this->doctrine = $doctrine;
+    }
+
+
     #[Route('/formulaires', name: 'app_formulaires')]
     public function index(): Response
     {
@@ -19,7 +30,7 @@ class FormulairesController extends AbstractController
     }
 
     #[Route('/formulaires/aeroport/afficher')]
-    public function aeroportAfficher(Request $req, ManagerRegistry $doctrine){
+    public function aeroportAfficher(Request $req){
         //on cree une etntié vide
         $aeroport = new Aeroport();
 
@@ -31,7 +42,7 @@ class FormulairesController extends AbstractController
 
         //si c'est POST on va visualiser le contenu de l'entité
         if ($form->isSubmitted() && $form->isValid()){
-            $em = $doctrine->getManager();
+            $em = $this->doctrine->getManager();
             $em->persist($aeroport);
             $em->flush();
             // dd($aeroport);
@@ -49,6 +60,72 @@ class FormulairesController extends AbstractController
         return $this->render('formulaires/aeroport_afficher.html.twig', $vars);
     }
 
+
+    //action qui affiche tous les aeroports
+    #[Route('/formulaires/afficher/aeroports', name:'afficherAeroports')]
+    public function afficherAeroports(){
+        //obtenir tous les aeroports de la BD
+        $em = $this->doctrine->getManager();
+        // $rep = $em->getRepository(Aeroport::class);
+        // $aeroports = $rep->findAll();
+        // //ou
+        $aeroports = $em->getRepository(Aeroport::class)
+        ->findAll();
+        // dd($aeroports);
+
+
+        //envoyer l'array d'aeroports a la vue
+        $vars =['aeroports'=> $aeroports,
+                'message'=> 'hello hello'];
+
+        return $this->render ('formulaires/afficher_aeroports.html.twig', $vars);
+
+    }
+
+
+    //update de l'aeroport (affichage et tratement du formulaire)
+    #[Route('/formulaires/update/aeroport/{id}', name:'updateAeroport')]
+    public function updateAeroport(Request $req, AeroportRepository $rep, EntityManagerInterface $em){
+
+        $id = $req->get('id');
+        $aeroport = $rep->find ($id);
+        // dd($aeroport);
+    
+        $form = $this->createForm(AeroportType::class, $aeroport);
+
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted()){
+            //on a cliqué submit
+            $em->flush();
+            dd($aeroport);
+        }
+
+        $vars = ['form'=>$form];
+
+        return $this->render('formulaires/update_aeroport.html.twig', $vars);
+
+    }
+
+    #[Route('/formulaires/delet/aeroport/{id}', name:'deleteAeroport')]
+    public function deleteAeroport(Request $req, AeroportRepository $rep, EntityManagerInterface $em){
+        //obtenir l'id de l'aeroport à effacer
+        $id = $req->get('id');
+
+        //obtenir l'aeroport de la bd
+        $aeroport = $rep->find($id);
+
+        //lancer remove
+        $em->remove($aeroport);
+
+        //lancer flush
+        $em->flush();
+
+        //quoi faire après le delete?
+        //Dans ce cas rediriger vers l'affichage de tous les aeroports
+        return $this->redirectToRoute('afficherAeroports');
+
+    }
 
 
 }
---
+++ Symfony7Cours/ProjetModeleSymfony/src/Form/LivreType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Auteur;
+use App\Entity\Livre;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LivreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('prix')
+            ->add('description')
+            ->add('datePublication', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('isbn')
+            ->add('auteurs', EntityType::class, [
+                'class' => Auteur::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Livre::class,
+        ]);
+    }
+}

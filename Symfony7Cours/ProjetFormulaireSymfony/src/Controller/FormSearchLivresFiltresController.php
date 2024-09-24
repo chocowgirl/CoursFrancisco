6ffbfed7e63739a021abfed1868cc1614bc8b393
Jessica C <j.conrad@interface3.be>
2024-09-24 11:59:15 +0200

    matin avec AJAX et JSON recherche filtre et affichage reponse

--- Symfony7Cours/ProjetFormulaireSymfony/src/Controller/FormSearchLivresFiltresController.php
+++ Symfony7Cours/ProjetFormulaireSymfony/src/Controller/FormSearchLivresFiltresController.php
@@ -8,11 +8,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class FormSearchLivresFiltresController extends AbstractController
 {
     #[Route('/livres/search', name: 'livres_search')]
-    public function livresSearch(Request $req, LivreRepository $rep): Response
+    public function livresSearch(Request $req, LivreRepository $rep, SerializerInterface $serializer): Response
     {
         $form = $this->createForm(SearchFiltreLivresType::class);
 
@@ -20,19 +22,22 @@ class FormSearchLivresFiltresController extends AbstractController
 
         //gestion du submit du form
         if ($form->isSubmitted()){
-
             // dd($form->getData());
             // $livres=$rep->findAll();
+            $livres = $rep->livresEntresDeuxPrix($form->getData());
+            // dump ($form->getData()); // les filtres
+            // dd($livres); //le resultat de la requete
 
-            $livres= $rep->livresEntresDeuxPrix($form->getData());
-            // dd($livres);
-
-            $vars = ['livres'=>$livres];
-            // dd($livres);
-            // return $this->render('form_search_livres_filtres/livres_search_afficher.html.twig');
-            // $rep->findAll();
-            // Ceci (findAll) nous donne un array des objets
-            return $this->redirectToRoute('livres_search_afficher_resultat');
+            $livresJson = $serializer->serialize($livres, 'json');
+            // dd ($livresJson); //le resultat de la requete encodé en JSON
+            return new Response($livresJson);
+            // $vars = ['livres'=>$livres];
+            // // dd($livres);
+            // // return $this->render('form_search_livres_filtres/livres_search_afficher.html.twig');
+            // // $rep->findAll();
+            // // Ceci (findAll) nous donne un array des objets
+            // return $this->redirectToRoute('livres_search_afficher_resultat');
+            
         }
 
         $vars = ['form' => $form];
@@ -42,7 +47,8 @@ class FormSearchLivresFiltresController extends AbstractController
 
     #[Route('/livres/search/afficher/resultat', name: 'livres_search_afficher_resultat')]
     public function livresSearchAfficherResultat (){
-        return $this->render('form_search_livres_filtres/livres_search_afficher_resultat.html.twig');
+        // return $this->render('form_search_livres_filtres/livres_search_afficher_resultat.html.twig');
+        return $this->redirectToRoute('livres_search_afficher_resultat');
 
     }
 

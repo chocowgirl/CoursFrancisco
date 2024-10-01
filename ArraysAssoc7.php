c0376f313e07fc699ea49ffab78e94252e35ef0c
JC <jessica.l.conrad@gmail.com>
2024-10-01 18:20:09 +0200

    putting home to update repo

--- ArraysAssoc7.php
+++ ArraysAssoc7.php
@@ -22,6 +22,25 @@
     }
 
 
+    $recipes = [
+        [
+            'title' => 'Cassoulet',
+            'recipe' => '',
+            'author' => 'mickael.andrieu@exemple.com',
+            'is_enabled' => true,
+        ],
+        [
+            'title' => 'Couscous',
+            'recipe' => '',
+            'author' => 'mickael.andrieu@exemple.com',
+            'is_enabled' => false,
+        ],
+    ];
+    
+    echo '<pre>';
+    print_r($recipes);
+    echo '</pre>';
+
     ?>
 </body>
 </html>
